084d75afae0a0fbea2b90b62343e9ea444361f83
Erwin <108355646+Erwin0925@users.noreply.github.com>
2024-12-17 00:05:34 +0800

    Add project files.

---
+++ CS_Practise/Program.cs
@@ -0,0 +1,13 @@
+﻿using CS_Practise.Question;
+
+namespace CS_Practise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            question5 ques = new question5();
+            ques.reverse();
+        }
+    }
+}
