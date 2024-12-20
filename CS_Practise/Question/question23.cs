1a9ae6b22df9b15a42d14053142461c3ef3df8f2
Erwin <108355646+Erwin0925@users.noreply.github.com>
2024-12-21 00:28:37 +0800

    Csharp basic question for quick refresh

--- CS_Practise/Question/question23.cs
+++ CS_Practise/Question/question23.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Implement a stack with push and pop operations.
+// Find the top element of the stack and check if the stack is empty or not.
+// FILO
 
 namespace CS_Practise.Question
 {
     internal class question23
     {
+        public void Stack()
+        {
+            Stack<int> IntStack = new Stack<int>();
+
+            IntStack.Push(0);
+            IntStack.Push(1);
+            IntStack.Push(2);
+
+            Console.Write("Initial Stack : ");
+            foreach (int i in IntStack)
+            {
+                Console.Write(i + ",");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine($"Top item is : {IntStack.Peek()}");
+
+            Console.WriteLine($"Popped item : {IntStack.Pop()}"); // display the top item and remove from the stack
+
+            Console.Write("IntStack after pop : ");
+            foreach (int i in IntStack)
+            {
+                Console.Write(i + ",");
+            }
+            Console.WriteLine();
+        }
     }
 }
---
+++ CS_Practise/Question/question24.cs
@@ -0,0 +1,21 @@
+﻿//sort the array in ascending order
+namespace CS_Practise.Question
+{
+    internal class question24
+    {
+        public void SortArrAscending()
+        {
+            int[] numns = { 5, 7, 3, 8, 10, 9 };
+            Console.WriteLine($"Original array: {string.Join(", ", numns)}");
+            SortAsc(numns);
+            Console.WriteLine($"Sorted array: {string.Join(", ", numns)}");
+        }
+
+        public static int[] SortAsc(int[] arr)
+        {
+            Array.Sort(arr, (a, b) => a.CompareTo(b));
+            return arr;
+            //return Array.Sort(arr) //this is another simplier method
+        }
+    }
+}
