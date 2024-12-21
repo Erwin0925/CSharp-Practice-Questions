c8f06f81a7459b347e326abd7c2d2a6c2f803220
Erwin <108355646+Erwin0925@users.noreply.github.com>
2024-12-21 19:13:54 +0800

    Understand the Use of Structure - Practise

---
+++ CS_Practise/Question/Struct/question40.cs
@@ -0,0 +1,39 @@
+﻿// 1. Write a program in C# Sharp to declare a simple structure.
+// Expected Output: The sum of x and y is 40 
+
+
+namespace CS_Practise.Question.Struct
+{
+
+    struct Point
+    {
+        public int x;
+        public int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int PointSum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+    internal class question40
+    {
+        public void getSum()
+        {
+            Point point = new Point();
+            point.x = 2;
+            point.y = 3;
+            var sum = point.PointSum(4, 6);
+
+            Console.WriteLine(point.x);
+            Console.WriteLine(point.y);
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ CS_Practise/Question/Struct/question41.cs
@@ -0,0 +1,34 @@
+﻿namespace CS_Practise.Question.Struct
+{
+    public struct book
+    {
+        public required string title;
+        public required string author;
+    }
+    internal class question41
+    {
+        public void ShowBook()
+        {
+            book[] arr = new book[3];
+            arr[0] = new book { title = "Sample Title0", author = "Sample Author0" };
+            arr[1] = new book { title = "Sample Title1", author = "Sample Author1" };
+            arr[2] = new book { title = "Sample Title2", author = "Sample Author2" };
+
+            foreach (book book in arr)
+            {
+                Console.WriteLine(book.title + " " + book.author);
+            }
+
+            List<book> list = new List<book>();
+            list.Add(new book { title = "Sample Title0", author = "Sample Author0" });
+            list.Add(new book { title = "Sample Title1", author = "Sample Author1" });
+            list.Add(new book { title = "Sample Title2", author = "Sample Author2" });
+
+            foreach (book book in list)
+            {
+                Console.WriteLine(book.title + " " + book.author);
+            }
+
+        }
+    }
+}
