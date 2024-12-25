778be725dd9ee5520180e06c881763224893e66d
Inspiron <Inspiron@DELL>
2024-12-26 00:45:12 +0800

    Delegate Practise Question

--- CS_Practise/Question/Delegate/question48.cs
+++ CS_Practise/Question/Delegate/question48.cs
@@ -1,5 +1,4 @@
-﻿//Dynamic Method Selection
-
+﻿//Dynamic Method Selection :
 //Define a delegate MathOperation for methods that take two integers and return an integer.
 //Write methods for:
     //Addition
@@ -12,9 +11,52 @@ namespace CS_Practise.Question.Delegate
 {
     internal class question48
     {
+        public delegate int MathOperation(int x, int y);  
+
+        public int Addition(int x, int y)
+        {
+            return x + y;
+        }
+        public int Subtraction(int x, int y)
+        {
+            return x - y;
+        }
+        public int Multiplication(int x, int y)
+        {
+            return x * y;
+        }
+        public int Division(int x, int y)
+        {
+            return x/y;
+        }
+
         public void DelegatePrac()
         {
+            MathOperation? mathopeation = null;
 
+            String choice = "Addition";
+
+            switch (choice)
+            {
+                case "Addition":
+                    mathopeation = Addition;
+                    break;
+                case "Subtraction":
+                    mathopeation = Subtraction;
+                    break;
+                case "Multiplication":
+                    mathopeation = Multiplication;
+                    break;
+                case "Division":
+                    mathopeation = Division;
+                    break;
+                default :
+                    Console.WriteLine($"{choice} is not an option");
+                    break;
+            }
+
+            int result = mathopeation(4, 5);
+            Console.Write($"{choice} : {result}");
         }
     }
 }
